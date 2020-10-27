9afcd6b9776f22ca06b2a121a8dc4042cf47a00a
Sperthix <matusbuci@gmail.com>
2020-10-27 13:38:26 +0100

    GUI dokoncene, logika funguje

--- main.py
+++ main.py
@@ -8,29 +8,55 @@ def prihlasenie():
     meno = vstup_meno.get() + "\n"
     heslo = vstup_heslo.get() + "\n"
     for x in f:
-        # print("Aktualny zaznam je: " + x)
-        # print("Zadane meno je: " + meno)
+        print("Aktualny zaznam je: " + x)
+        print("Zadane meno je: " + meno)
         if x == meno:
-            # print("rozpoznal som meno")
+            print("rozpoznal som meno")
             y = f.readline()
             if y == heslo:
-                label_info.config(text="Prihlásenie bolo úspešné.")
+                label_kontrola.config(text="Prihlásenie bolo úspešné.")
                 uspech = True
                 break
             else:
-                label_info.config(text="Nesprávne heslo, zadajte prosím správne meno a heslo.")
-                prihlasenie()
+                label_kontrola.config(text="Nesprávne heslo, zadajte prosím správne meno a heslo.")
+                prihlasenie_gui()
     if not uspech:
+        f.close()
         print("Prihlasenie nebolo uspesne, navrat do hlavneho menu")
+        label_kontrola.config(text="Nemvydalo")
         menu()
-    f.close()
-    menu()
+
+    else:
+        f.close()
+        label_kontrola.config(text="Access granted!")
+        menu()
+
+def prihlasenie_gui():
+    tlacidlo_login.grid_forget()
+    tlacidlo_spat.grid_forget()
+    label_info.grid_forget()
+
+    label_info.config(text="Pre prihlásenie prosím zadajte Vaše meno a heslo.")
+    label_meno.config(text="Meno: ")
+    label_heslo.config(text="Heslo: ")
+
+    tlacidlo_login.config(text="Login", command=prihlasenie)
+    tlacidlo_spat.config(text="Spat", command=menu)
+
+    label_info.grid(row=3, columnspan=2)
+    label_meno.grid(row=0, column=0, sticky=E)
+    label_heslo.grid(row=1, column=0, sticky=E)
+    vstup_meno.grid(row=0, column=1)
+    vstup_heslo.grid(row=1, column=1)
+    tlacidlo_login.grid(row=2, column=1)
+    tlacidlo_spat.grid(row=2, column=0)
 
 
 def registracia():
     print("Vytvorenie noveho prihlasovacieho konta")
-    meno = input("Zadajte prihlasovacie meno: ")
-    heslo = input("Zadajte heslo: ")
+    meno = vstup_meno.get()
+    heslo = vstup_heslo.get()
+    print("meno: " + meno + "\n" + "heslo: " + heslo)
     f = open("login.txt", "a")
     f.writelines(meno + "\n")
     f.writelines(heslo + "\n")
@@ -40,42 +66,59 @@ def registracia():
     menu()
 
 
+def registracia_gui():
+    tlacidlo_login.grid_forget()
+    tlacidlo_spat.grid_forget()
+    label_info.grid_forget()
+
+    label_info.config(text="Vytvorenie noveho uctu:")
+    label_meno.config(text="Zadajte uzivatelske meno: ")
+    label_heslo.config(text="Zadajte heslo: ")
+
+    tlacidlo_login.config(text="Registruj", command=registracia)
+    tlacidlo_spat.config(text="Spat", command=menu)
+
+    label_info.grid(row=0, columnspan=2)
+    label_meno.grid(row=1, column=0, sticky=E)
+    label_heslo.grid(row=2, column=0, sticky=E)
+    vstup_meno.grid(row=1, column=1)
+    vstup_heslo.grid(row=2, column=1)
+    tlacidlo_login.grid(row=3, column=1)
+    tlacidlo_spat.grid(row=3, column=0)
+
+
 def menu():
-    i = input("Chcete sa prihlasit (1), vytvorit ucet? (2), alebo ukoncit program? (0)")
-    if i.isdigit():
-        if int(i) == 1:
-            prihlasenie()
-        elif int(i) == 2:
-            registracia()
-        elif int(i) == 0:
-            sys.exit()
-        else:
-            print("nesprávny vstup")
-            menu()
-    else:
-        print("Zadaj cislo")
-        menu()
+    label_info.grid_forget()
+    label_meno.grid_forget()
+    label_heslo.grid_forget()
+    vstup_meno.grid_forget()
+    vstup_heslo.grid_forget()
+    tlacidlo_login.grid_forget()
+    tlacidlo_spat.grid_forget()
+
+    label_info.config(text="Vitajte v prihlasovacom systeme!")
+    tlacidlo_login.config(text="Prihlasenie", command=prihlasenie_gui)
+    tlacidlo_spat.config(text="Registracia", command=registracia_gui)
+
+    label_info.grid(row=0, columnspan=2, column=0)
+    tlacidlo_login.grid(row=1, column=0)
+    tlacidlo_spat.grid(row=1, column=1)
 
 
 login = Tk()
 
-label_info = Label(login, text="Pre prihlásenie prosím zadajte Vaše meno a heslo.")
-label_meno = Label(login, text="Meno: ")
-label_heslo = Label(login, text="Heslo: ")
+label_info = Label(login)
+label_meno = Label(login)
+label_heslo = Label(login)
+label_kontrola = Label(login, text="Ja som kontrola, dont mind me")
 
 vstup_meno = Entry(login)
 vstup_heslo = Entry(login)
 
-tlacidlo_login = Button(login, text="Login", command=prihlasenie)
+tlacidlo_login = Button(login)
+tlacidlo_spat = Button(login)
 
-label_info.grid(row=3, columnspan=2)
-label_meno.grid(row=0, column=0, sticky=E)
-label_heslo.grid(row=1, column=0, sticky=E)
-vstup_meno.grid(row=0, column=1)
-vstup_heslo.grid(row=1, column=1)
-tlacidlo_login.grid(row=2, columnspan=2)
+label_kontrola.grid(row=10, columnspan=3)
 
-
-login.mainloop()
 menu()
-
+login.mainloop()
